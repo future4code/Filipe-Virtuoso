c4973cf45517f20741e780f8d397ffee881a1f98
Filipe Virtuoso <lfvirtuoso@gmail.com>
2020-02-07 12:37:50 -0300

    Adicionar exercícios de interpretação

---
+++ semana4/lista-de-exercicios/assets/js/main.js
@@ -0,0 +1,26 @@
+// Exercícios de interpretação
+
+/*
+Exercício 1 --
+A funçao recebe um valor em dólares, depois é informada a cotação e a função
+retorna o valor convertido para reais.
+
+Exercício 2 --
+165
+"TIPO DE INVESTIMETO INFORMADO INCORRETO!"
+
+Exercício 3 --
+14
+6
+8
+
+Exercício 4 --
+-10
+1590
+*/
+
+// Exercícios de lógica
+
+/*
+Exercício 1 --
+*/
