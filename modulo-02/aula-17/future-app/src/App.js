658d42a3a0d06a13e23219c650a0e65bcecb2851
Filipe Virtuoso <lfvirtuoso@gmail.com>
2020-08-07 06:33:16 -0300

    Adicionar future-tube

--- modulo-02/aula-17/future-app/src/App.js
+++ modulo-02/aula-17/future-app/src/App.js
@@ -1,25 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav id="navbar" className="navbar">
+        <h1 className="logo">
+          <a href="index.html">
+            <i class="fab fa-youtube"></i> FUTURE TUBE
+          </a>
+        </h1>
+        <div class="search-input">
+          <input type="text" placeholder="Pesquise um vídeo" />
+          <a href="index.html">
+            <i class="fas fa-search"></i>
+          </a>
+        </div>
+        <ul className="menu">
+          <li>
+            <a href="index.html" class="active">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="index.html">Em Alta</a>
+          </li>
+          <li>
+            <a href="index.html">Meu canal</a>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <div className="container">
+          <aside className="sidebar">
+            <ul>
+              <li>
+                <a href="index.html">item 1</a>
+              </li>
+              <li>
+                <a href="index.html">item 2</a>
+              </li>
+              <li>
+                <a href="index.html">item 3</a>
+              </li>
+              <li>
+                <a href="index.html">item 4</a>
+              </li>
+              <li>
+                <a href="index.html">item 5</a>
+              </li>
+            </ul>
+          </aside>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+        </div>
+      </main>
+      <footer className="footer">Feito com amor por Filipe Virtuoso</footer>
+    </>
   );
 }
 
